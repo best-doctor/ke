67e0f338d53bb261506d311591f97868798beb63
Philipp Kuznetsov <philipp@bestdoctor.ru>
2020-02-29 15:46:24 +0300

    Add initial layout module

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import LayoutComposer from "./LayoutComposer";
 
-function App() {
-  return <Container>
-    <Row>
-      <Col>Hello world</Col>
-    </Row>
-  </Container>;
-}
+const App = () => <LayoutComposer />;
 
 export default App;
---
+++ src/LayoutComposer/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+const View = () => <div>Returns composed HTML</div>;
+
+export default View;
