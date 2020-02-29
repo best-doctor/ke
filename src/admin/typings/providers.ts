de3e2a1b49242fe1870f94ba4e47233751adc005
Maxim Khaikin <m.khaikin@bestdoctor.ru>
2020-02-29 17:07:50 +0300

    extend BaseProvider[2]

--- src/admin/typings/providers.ts
+++ src/admin/typings/providers.ts
@@ -1,1 +1,14 @@
-export abstract class BaseProvider {}
+abstract class BaseProvider {
+    url!: string;
+    writableFields!: string[];
+    readOnlyFields!: string[];
+
+    abstract getList: () => Object[];
+    abstract getObject: (objectId: string) => Object;
+
+    abstract create = (data: Object) => Object;
+    abstract update = (data: Object) => Object;
+    abstract delete = (data: Object) => Object;
+};
+
+export { BaseProvider };
