5e8729ab67c503c9899108ba78c8cfd731344a5b
mcproger <mcproger7@gmail.com>
2020-04-17 13:05:10 +0300

    Update http requests with env variables usage

--- src/admin/providers/index.ts
+++ src/admin/providers/index.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
-const backendAPIURL = '/api'
+const backendAPIURL = process.env.API_URL
+const authHeader = process.env.AUTH_HEADER
+
+const HTTP = axios.create({
+  headers: {
+    Authorization: authHeader,
+  },
+})
 
 export abstract class BaseProvider {
   abstract url: string
@@ -10,12 +17,12 @@ export abstract class BaseProvider {
   abstract readOnlyFields: string[]
 
   getList = async (): Promise<Model[]> => {
-    const response = await axios.get(backendAPIURL + this.url)
-    return response.data
+    const response = await HTTP.get(backendAPIURL + this.url)
+    return response.data.data
   }
 
   getObject = async (objectId: string): Promise<Model> => {
-    const response = await axios.get(`${backendAPIURL + this.url + objectId}/`)
-    return response.data
+    const response = await HTTP.get(`${backendAPIURL + this.url + objectId}/`)
+    return response.data.data
   }
 }
