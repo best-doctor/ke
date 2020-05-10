e6c6d099c9e5c26830ec6bac5c71b450a77fe0fd
mcproger <mcproger7@gmail.com>
2020-05-10 15:52:00 +0300

    Simplify test object structure

--- src/utils/tests/mountComponents.test.jsx
+++ src/utils/tests/mountComponents.test.jsx
@@ -3,29 +3,10 @@ import { Text } from '@chakra-ui/core'
 import { mountComponents } from '../mountComponents'
 
 const detailObject = {
-  category: 100500,
   id: 100500,
-  notice: "alks;djf;alksdjf;alksdjfasdfasdf",
   patient: {
-    appeals_url: "",
-    email: "user3061@test.com",
-    first_name: "Мария",
     full_name: "Кошелева Мария Егоровна",
-    last_name: "Кошелева",
-    loyalty: null,
-    middle_name: "Егоровна",
-    notes: "",
-    phone: "+79210002124",
-    priority: "Обычный",
-    status: "Создан",
-    url: "http://localhost:9000/experimental_api/patients/f1152a0d-6af8-4078-89bc-5cce67d63e93/",
-    uuid: "f1152a0d-6af8-4078-89bc-5cce67d63e93",
   },
-  program: { id: 426, title: "Test", holding: "Test" },
-  result: { result: 'success', text: 'Успешно' },
-  status: { value: "delayed", text: "Отложено" },
-  url: "http://localhost:9000/experimental_api/appeals/22/",
-  user: { id: 5264, email: "test@admin.com" },
 }
 
 const adminFields = [
