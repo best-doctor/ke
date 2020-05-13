ba6f667cce4aeb2365283d5381d3b2e3df6cf180
mcproger <mcproger7@gmail.com>
2020-05-13 21:41:47 +0300

    Fix yarn format

--- src/admin/providers/index.ts
+++ src/admin/providers/index.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 const HTTP = axios.create({
   withCredentials: true,
 })
--- src/utils/tests/mountComponents.test.jsx
+++ src/utils/tests/mountComponents.test.jsx
@@ -5,16 +5,16 @@ import { mountComponents } from '../mountComponents'
 const detailObject = {
   id: 100500,
   patient: {
-    full_name: "Кошелева Мария Егоровна",
+    full_name: 'Кошелева Мария Егоровна',
   },
 }
 
 const adminFields = [
   {
-    name: "patient__full_name",
+    name: 'patient__full_name',
     widget: Text,
     widget_attrs: {
-      color: "teal.500",
+      color: 'teal.500',
     },
     layout: { x: 2, y: 2, w: 3, h: 1, static: true },
   },
@@ -25,8 +25,8 @@ test('Get components to mount', () => {
   const component = components[0]
 
   expect(component.type.displayName).toEqual('Text')
-  expect(component.key).toEqual("100500patient__full_name")
-  expect(component.props.color).toEqual("teal.500")
-  expect(component.props["data-grid"]).toEqual({ x: 2, y: 2, w: 3, h: 1, static: true })
-  expect(component.props.children).toEqual("Кошелева Мария Егоровна")
+  expect(component.key).toEqual('100500patient__full_name')
+  expect(component.props.color).toEqual('teal.500')
+  expect(component.props['data-grid']).toEqual({ x: 2, y: 2, w: 3, h: 1, static: true })
+  expect(component.props.children).toEqual('Кошелева Мария Егоровна')
 })
