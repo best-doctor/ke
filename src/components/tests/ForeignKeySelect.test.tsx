abacb846950dab34a0eb33b4b802b96f2a02ae2d
mcproger <mcproger7@gmail.com>
2020-07-09 16:25:48 +0300

    Style fixes

--- src/components/tests/ForeignKeySelect.test.tsx
+++ src/components/tests/ForeignKeySelect.test.tsx
@@ -9,8 +9,8 @@ test('FK select renders async select', () => {
   const component = mount(
     <ForeignKeySelect
       provider={testProvider}
-      dataResourceUrl='/tests/'
-      placeholder='test'
+      dataResourceUrl="/tests/"
+      placeholder="test"
       handleChange={(value: any) => value.id}
       getOptionLabel={(option: any) => option.id}
       getOptionValue={(option: any) => option.id}
