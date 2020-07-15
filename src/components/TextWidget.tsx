f298c1601a6131404ba858c636f8a681d562b4c1
mcproger <mcproger7@gmail.com>
2020-07-15 18:31:14 +0300

    Generic text widget

--- src/components/TextWidget.tsx
+++ src/components/TextWidget.tsx
@@ -1,23 +1,40 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { Text } from '@chakra-ui/core'
+import { Text, FormLabel, Box } from '@chakra-ui/core'
+import { getWidgetContent } from '../utils/dataAccess'
 
 const StyledTextWidget = styled.div`
   border-width: 2px;
   border-radius: 3px;
   border-color: #cbd5e0;
-  padding-left: 2px;
+  padding-left: 5px;
   padding-right: 2px;
   padding-top: 2px;
+  padding-bottom: 2px;
 `
 
-const TextWidget = (props: any): JSX.Element => {
-  const { children } = props
+type TextWidgetProps = {
+  name: string
+  detailObject: any
+  helpText: string
+  displayValue: string | Function | undefined
+  style: any
+}
+
+const TextWidget = ({ name, detailObject, displayValue, helpText, style }: TextWidgetProps): JSX.Element => {
+  const content = getWidgetContent(name, detailObject, displayValue)
 
   return (
-    <StyledTextWidget {...props}>
-      <Text>{children}</Text>
-    </StyledTextWidget>
+    <Box {...style}>
+      {content && (
+        <>
+          <FormLabel>{helpText || ''}</FormLabel>
+          <StyledTextWidget>
+            <Text>{content}</Text>
+          </StyledTextWidget>
+        </>
+      )}
+    </Box>
   )
 }
 
