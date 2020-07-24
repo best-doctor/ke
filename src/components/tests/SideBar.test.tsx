04fb1acbc0a3acc90d5e0992d1df09280b900aff
mcproger <mcproger7@gmail.com>
2020-07-24 17:01:05 +0300

    Bump test coverage

---
+++ src/components/tests/SideBar.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { Menu } from 'react-feather'
+import { Drawer, DrawerContent } from '@chakra-ui/core'
+
+import { SideBar } from '../SideBar'
+
+test('Side bar component properly rendered', () => {
+  const component = shallow(
+    <SideBar header="Test">
+      <h1>Test</h1>
+      <h1>Test2</h1>
+    </SideBar>
+  )
+
+  expect(component.find(Menu).length).toEqual(1)
+  expect(component.find(Drawer).length).toEqual(1)
+  expect(component.find(DrawerContent).length).toEqual(1)
+  expect(component.find('h1').length).toEqual(2)
+})
---
+++ src/components/tests/TextWidget.test.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { FormLabel, Box, Text } from '@chakra-ui/core'
+
+import { TextWidget } from '../TextWidget'
+
+const detailObject = {
+  test: {
+    name: 'test',
+  },
+}
+
+test('Text widget properly rendered', () => {
+  const component = shallow(
+    <TextWidget name="test.name" helpText="test" style={{}} displayValue={undefined} detailObject={detailObject} />
+  )
+
+  expect(component.find(Box).length).toEqual(1)
+  expect(component.find(Text).length).toEqual(1)
+  expect(component.find(FormLabel).length).toEqual(1)
+})
