e029cb058d42ba73f790a85d7887a9ee37381ebe
mcproger <mcproger7@gmail.com>
2020-07-27 12:22:58 +0300

    Update is functional component checking

--- src/utils/isComponent.ts
+++ src/utils/isComponent.ts
@@ -1,8 +1,6 @@
 // This shit is stolen from several libs like react-table
 // because I could not find a normal way in the official documentation!!!
 
-const FUNCTION_REGEX = /react(\d+)?./i
-
 const isClassComponent = (objectToCheck: any): boolean => {
   return (
     typeof objectToCheck === 'function' &&
@@ -17,7 +15,6 @@ const isFunctionalComponent = (objectToCheck: any): boolean => {
   return (
     typeof objectToCheck === 'function' &&
     String(objectToCheck).includes('return') &&
-    !!FUNCTION_REGEX.exec(String(objectToCheck)) &&
     String(objectToCheck).includes('.createElement')
   )
 }
