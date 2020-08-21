e40068497ac14f197c2f87943d6854b19fd4ff72
mcproger <mcproger7@gmail.com>
2020-08-21 14:27:40 +0300

    Better typing

--- src/DetailView/utils/mountDetailFields.ts
+++ src/DetailView/utils/mountDetailFields.ts
@@ -1,9 +1,27 @@
 import { createStore, createEvent } from 'effector'
 
+import type { DetailFieldDescription } from 'admin/fields/FieldDescription'
+
+import type { BaseProvider } from 'index'
+import type { BaseNotifier } from 'common/notifier'
+import type { BaseAnalytic } from 'integration/analytics'
+
 import { mountComponents } from '../../common/utils/mountComponents'
 import { makeUpdateWithNotification } from '../../admin/providers/utils'
 
 type WidgetPayload = { url: string; payload: object }
+
+type MountDetailFieldsArgs = {
+  resourceName: string
+  object: object
+  elements: DetailFieldDescription[]
+  provider: BaseProvider
+  setObject: Function
+  notifier: BaseNotifier
+  user: object
+  analytics: BaseAnalytic
+  ViewType: string
+}
 
 const containerStore = createStore<object>({})
 const submitChange = createEvent<WidgetPayload>()
@@ -19,7 +37,7 @@ const mountDetailFields = ({
   user,
   analytics,
   ViewType,
-}: any) => {
+}: MountDetailFieldsArgs): JSX.Element[] => {
   containerStore
     .on(setInitialValue, (state: object, value: object) => ({ ...state, ...value }))
     .on(submitChange, (_, widgetPayload: WidgetPayload) => {
