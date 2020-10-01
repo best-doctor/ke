bb8af666f3fd5bc6f8ffc8811be4e05f7223a356
Yakov <yakov@istomin.me>
2020-10-01 12:34:15 +0300

    Export ValidationWrapper

--- src/index.tsx
+++ src/index.tsx
@@ -56,6 +56,7 @@ import {
 } from './ListView/components/Table/filters'
 import { AsyncSelectWidget } from './common/components/AsyncSelectWidget'
 import { WidgetWrapper } from './common/components/WidgetWrapper'
+import { ValidationWrapper } from './common/components/ValidationWrapper'
 
 export {
   BaseAdmin,
@@ -114,4 +115,5 @@ export {
   WidgetProps,
   handleUserAction,
   DateTimeWidget,
+  ValidationWrapper,
 }
