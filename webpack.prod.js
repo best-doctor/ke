f70385412d8efdc881a427cee4b67a7d4cc073f4
Alexander Bezrukov <abezrukov@bestdoctor.ru>
2021-01-19 16:23:34 +0300

    Has add size limits for production bundle. Try to keep it or minimize.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const maxBundleBytesSize = 1500 * 1024;
+
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  performance: {
+    hints: 'error',
+    maxEntrypointSize: maxBundleBytesSize,
+    maxAssetSize: maxBundleBytesSize
+  },
 })
