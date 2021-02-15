547fbc61524b9fc5d670f6250b826eb87cca8f0b
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2021-02-15 16:43:31 +0300

    DateTime widgets - change events tests and fix.

--- src/DetailView/tests/tests_widgets/DateTimeRangeWidget.test.tsx
+++ src/DetailView/tests/tests_widgets/DateTimeRangeWidget.test.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { act } from 'react-dom/test-utils'
+import { ThemeProvider } from '@chakra-ui/core'
 
 import { WidgetWrapper } from '../../../common/components/WidgetWrapper'
 import { DateTimeRangeWidget } from '../../widgets/DateTimeRangeWidget'
@@ -15,31 +17,50 @@ const detailObject = {
 }
 
 const getComponent = (): JSX.Element => (
-  <DateTimeRangeWidget
-    name="test"
-    resource="test-resource"
-    analytics={undefined}
-    widgetAnalytics={jest.fn()}
-    helpText="test"
-    mainDetailObject={detailObject}
-    dataSource={jest.fn()}
-    setMainDetailObject={jest.fn()}
-    displayValue="2021-01-01"
-    dataTarget="https://some-test-target.com"
-    targetPayload={(value: string) => ({ testPayload: value })}
-    notifier={testNotifier}
-    provider={testProvider}
-    viewType="test_view"
-    style={{}}
-    setInitialValue={jest.fn()}
-    submitChange={submitChangeMock}
-    containerStore={mockedEffectorContainerStore}
-  />
+  <ThemeProvider>
+    <DateTimeRangeWidget
+      name="test"
+      resource="test-resource"
+      analytics={undefined}
+      widgetAnalytics={jest.fn()}
+      helpText="test"
+      mainDetailObject={detailObject}
+      dataSource={jest.fn()}
+      setMainDetailObject={jest.fn()}
+      displayValue={() => ['2021-01-01T00:00:00', '2021-01-02T00:00:00']}
+      dataTarget="https://some-test-target.com"
+      targetPayload={(value: string) => ({ testPayload: value })}
+      notifier={testNotifier}
+      provider={testProvider}
+      viewType="test_view"
+      style={{}}
+      setInitialValue={jest.fn()}
+      submitChange={submitChangeMock}
+      containerStore={mockedEffectorContainerStore}
+    />
+  </ThemeProvider>
 )
 
 test('DateTimeRangeWidget properly rendered', () => {
-  const component = shallow(getComponent())
+  const component = mount(getComponent())
 
   expect(component.find(BaseDateTimeRangeWidget).length).toEqual(1)
   expect(component.find(WidgetWrapper).length).toEqual(1)
+})
+
+test('DateTimeRangeWidget properly handle event', () => {
+  const component = mount(getComponent())
+
+  act(() => {
+    component
+      .find(BaseDateTimeRangeWidget)
+      .first()
+      .props()
+      .handleChangeDate(new Date('2021-02-02T00:00:00'), 'end', true)
+  })
+
+  expect(submitChangeMock).toHaveBeenCalledWith({
+    url: 'https://some-test-target.com',
+    payload: { testPayload: ['2021-01-01T00:00:00', '2021-02-02T00:00:00'] },
+  })
 })
