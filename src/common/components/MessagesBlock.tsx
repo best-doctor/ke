e366c789c202c7fa3f78639662eff0ee38d25e83
LerikP <lerikpav@gmail.com>
2021-04-15 09:03:48 +0300

    Improve validation messages style

--- src/common/components/MessagesBlock.tsx
+++ src/common/components/MessagesBlock.tsx
@@ -1,10 +1,19 @@
+import { Box, Text } from '@chakra-ui/core'
 import React from 'react'
+import { AlertCircle, CheckCircle, HelpCircle, Icon, XCircle } from 'react-feather'
 
 const messageTypeMapping: { [key: string]: string } = {
-  success: 'green',
-  warning: 'orange',
-  error: 'red',
-  info: 'black',
+  success: '#65BBBB',
+  warning: '#FFEAB3',
+  error: '#FC6363',
+  info: '#81C2FE',
+}
+
+const messageIconMapping: { [key: string]: Icon } = {
+  success: CheckCircle,
+  warning: AlertCircle,
+  error: XCircle,
+  info: HelpCircle,
 }
 
 const MessagesBlock = ({
@@ -17,20 +26,22 @@ const MessagesBlock = ({
   if (typeof messages === 'undefined') return <></>
 
   const messageColor = messageTypeMapping[messageType]
+  const messageIcon = messageIconMapping[messageType]
 
   return (
-    <ul>
+    <>
       {messages
         .filter((message: string | null) => message !== null)
         .map((message: string, index) => {
           const key = index
           return (
-            <li style={{ color: messageColor }} key={key}>
-              {message}
-            </li>
+            <Box bg={messageColor} key={key} display="flex" alignItems="center" borderRadius="4px" m={4}>
+              <Box as={messageIcon} size="1.5em" m={4} flexShrink={0} />
+              <Text>{message}</Text>
+            </Box>
           )
         })}
-    </ul>
+    </>
   )
 }
 
