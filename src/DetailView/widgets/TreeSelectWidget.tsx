8a1b7bd5b5453e9e8f89ac6437ae2a87bd74652e
GitAlmaz <almazinhs@gmail.com>
2021-05-11 16:59:30 +0600

    treeSelect styles

--- src/DetailView/widgets/TreeSelectWidget.tsx
+++ src/DetailView/widgets/TreeSelectWidget.tsx
@@ -7,6 +7,7 @@ import { useWidgetInitialization } from '../../common/hooks/useWidgetInitializat
 import { ValidationWrapper } from '../../common/components/ValidationWrapper'
 import { WidgetWrapper } from '../../common/components/WidgetWrapper'
 import { getPayload } from '../utils/dataAccess'
+import { createGlobalStyle } from 'styled-components'
 import 'rc-tree-select/assets/index.less'
 
 type ForeignKeySelectWidgetProps = WidgetProps & {
@@ -25,6 +26,68 @@ type TreeOptionObject = {
   isLeaf: boolean
   childrenUrl: string | null
 }
+const StyledTreeSelect = createGlobalStyle`
+  .rc-tree-select-single:not(.rc-tree-select-customize-input) .rc-tree-select-selector {
+    border: 1px solid #CDD5DF;
+    border-radius: 4px;
+  }
+  .rc-tree-select-selection-search-input {
+    background: rgb(255, 255, 255)!important;
+    padding: .1rem 1rem;
+    font-size: 16px;
+  }
+  .rc-tree-select-tree-switcher {
+    display: inline-block!important;
+  }
+  .rc-tree-select-tree-switcher_open,
+  .rc-tree-select-tree-switcher_close {
+    background-image: none!important;
+  }
+  .rc-tree-select-tree .rc-tree-select-tree-treenode span.rc-tree-select-tree-icon__close,
+  .rc-tree-select-tree .rc-tree-select-tree-treenode span.rc-tree-select-tree-icon__open,
+  .rc-tree-select-tree .rc-tree-select-tree-treenode span.rc-tree-select-tree-icon__docu {
+    display: none!important;
+  }
+  .rc-tree-select-dropdown {
+    background-color: white;
+    border: 1px solid #d9d9d9;
+    box-shadow: 0 0px 4px #d9d9d9;
+    border-radius: 4px;
+    box-sizing: border-box;
+    z-index: 100;
+    left: -9999px;
+    top: -9999px;
+    position: absolute;
+    outline: none;
+  }
+  .rc-tree-select-show-arrow .rc-tree-select-arrow {
+    top: .5rem;
+    transform: rotate(90deg);
+  }
+  .rc-tree-select-clear-icon {
+    font-size: 21px;
+    right: 1rem;
+  }
+  .rc-tree-select-arrow {
+    display: none;
+  }
+  .rc-tree-select-tree-node-selected {
+    background-color: #F1F2F4;
+    box-shadow: none;
+    opacity: 0.8;
+  }
+  .rc-tree-select-tree-treenode-selected {
+    background: #F1F2F4;
+  }
+  .rc-tree-select-single .rc-tree-select-selector .rc-tree-select-selection-item {
+    top: .1rem;
+    left: 1rem;
+    white-space: nowrap;
+    max-width: 85%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`
 
 /**
  * Create select component with async loading options filtered by input text
@@ -72,8 +135,49 @@ const TreeSelectWidget = (props: ForeignKeySelectWidgetProps): JSX.Element => {
   )
   setInitialValue(value ? getPayload(value, name, targetPayload) : null)
 
+  const arrowPath =
+    'M765.7 486.8L314.9 134.7c-5.3-4.1' +
+    '-12.9-0.4-12.9 6.3v77.3c0 4.9 2.3 9.6 6.1 12.6l36' +
+    '0 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6' +
+    '.7 7.7 10.4 12.9 6.3l450.8-352.1c16.4-12.8 16.4-3' +
+    '7.6 0-50.4z'
+
+  const getSvg = (path: string, iStyle = {}, style = {}) => {
+    return (
+      <i style={iStyle}>
+        <svg
+          viewBox="0 0 1024 1024"
+          width="1em"
+          height="1em"
+          fill="currentColor"
+          style={{ verticalAlign: '-.125em', ...style }}
+        >
+          <path d={path} />
+        </svg>
+      </i>
+    )
+  }
+
+  const switcherIcon = (obj: any) => {
+    if (obj.isLeaf) {
+      return getSvg('', { cursor: 'pointer', background: 'white' }, { transform: 'rotate(270deg)' })
+    }
+    return getSvg(
+      arrowPath,
+      { cursor: 'pointer', background: 'white' },
+      { transform: `rotate(${obj.expanded ? 90 : 0}deg)` }
+    )
+  }
+
+  const inputIcon = getSvg(arrowPath)
+  const iconProps = {
+    inputIcon,
+    switcherIcon,
+  }
   const getUrl = (paramName: string, changeValue: string): string => {
-    const url = new URL(dataResourceUrl)
+    console.log(dataResourceUrl)
+
+    const url = new URL('https://staging-svct.bestdoctor.ru/api/medical_program/service_tree_node/')
     url.searchParams.append(paramName, changeValue)
     return url.href
   }
@@ -158,6 +262,7 @@ const TreeSelectWidget = (props: ForeignKeySelectWidgetProps): JSX.Element => {
       useClipboard={useClipboard}
       notifier={notifier}
     >
+      <StyledTreeSelect />
       <ValidationWrapper
         notBlockingValidators={notBlockingValidators}
         blockingValidators={blockingValidators}
@@ -174,6 +279,7 @@ const TreeSelectWidget = (props: ForeignKeySelectWidgetProps): JSX.Element => {
           onSearch={handleSearch}
           showSearch
           allowClear
+          {...iconProps}
         />
       </ValidationWrapper>
     </WidgetWrapper>
