ceca795eb3e60673be4e2a9305420e122bbcdd7b
LerikP <lerikpav@gmail.com>
2021-05-27 13:32:08 +0300

    Bump version: Allow ContainerWidget to be collapsible

--- src/DetailView/widgets/ContainerWidget.tsx
+++ src/DetailView/widgets/ContainerWidget.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box } from '@chakra-ui/react'
+import { Store } from 'effector'
+
 import { mountComponents } from '../../common/utils/mountComponents'
+import { getAccessor } from '../utils/dataAccess'
 
 /**
  * Render props.widgets in data-grid by their layout-properties
@@ -24,25 +27,43 @@ const ContainerWidget = (props: any): JSX.Element => {
     containerStore,
     widgets,
     name,
+    isCollapsible = false,
+    helpText = '',
   } = props
-  return (
-    <Box data-name={name}>
-      {mountComponents({
-        setInitialValue,
-        submitChange,
-        resourceName,
-        mainDetailObject,
-        elements: widgets,
-        provider,
-        setMainDetailObject,
-        refreshMainDetailObject,
-        notifier,
-        user,
-        analytics,
-        ViewType,
-        containerStore,
-      })}
-    </Box>
+  const containerContent = mountComponents({
+    setInitialValue,
+    submitChange,
+    resourceName,
+    mainDetailObject,
+    elements: widgets,
+    provider,
+    setMainDetailObject,
+    refreshMainDetailObject,
+    notifier,
+    user,
+    analytics,
+    ViewType,
+    containerStore,
+  })
+  const context = (containerStore as Store<object>).getState()
+  const isContainerCollapsible = getAccessor(isCollapsible, mainDetailObject, context)
+
+  return isContainerCollapsible ? (
+    <Accordion allowToggle pt={4}>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              {helpText}
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>{containerContent}</AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  ) : (
+    <Box data-name={name}>{containerContent}</Box>
   )
 }
 
