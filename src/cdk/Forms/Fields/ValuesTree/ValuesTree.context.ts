db4f9aa5012e42c1f093d3a4b9112bde17bf3d04
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2021-06-11 08:39:18 +0300

    Throw error on using ContextTree without initialization

--- src/cdk/Forms/Fields/ValuesTree/ValuesTree.context.ts
+++ src/cdk/Forms/Fields/ValuesTree/ValuesTree.context.ts
@@ -1,4 +1,8 @@
 import { createContext } from 'react'
 import { RootContext } from '@cdk/ContextTree'
 
-export const ValuesContext = createContext<RootContext>([() => undefined, () => undefined])
+export const ValuesContext = createContext<RootContext>([defaultContextFunc, defaultContextFunc])
+
+function defaultContextFunc(): never {
+  throw new Error(`Try to use ValuesContext before initialization.`)
+}
--- src/cdk/Forms/Validation/ErrorsTree/ErrorsTree.context.ts
+++ src/cdk/Forms/Validation/ErrorsTree/ErrorsTree.context.ts
@@ -1,4 +1,8 @@
 import { createContext } from 'react'
 import { RootContext } from '@cdk/ContextTree'
 
-export const ErrorsContext = createContext<RootContext>([() => undefined, () => undefined])
+export const ErrorsContext = createContext<RootContext>([defaultContextFunc, defaultContextFunc])
+
+function defaultContextFunc(): never {
+  throw new Error(`Try to use ErrorsContext before initialization.`)
+}
