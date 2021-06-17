680474e47428ca2c21aed252f52c5c8ea783a941
LerikP <lerikpav@gmail.com>
2021-06-17 17:44:14 +0300

    Improve table body style

--- src/Widgets/Table/Table.tsx
+++ src/Widgets/Table/Table.tsx
@@ -2,10 +2,27 @@ import { makeTable } from '@cdk/Tables'
 import styled from 'styled-components'
 
 export const StyledTable = makeTable(
-  'table',
+  styled.table`
+    text-align: left;
+    width: 100%;
+    tbody {
+      vertical-align: top;
+    }
+  `,
   styled.tr`
     thead & {
-      background: #aaa;
+      background: #f9fafa;
+      border-bottom-width: 2px;
+      border-color: #e3e5e8;
     }
+    border-bottom-width: 1px;
+    border-color: #e3e5e8;
+  `,
+  styled.th`
+    height: 3em;
+    padding: 0.5em 3em 0.5em 0.5em;
+  `,
+  styled.td`
+    padding: 1em 3em 1em 0.5em;
   `
 )
