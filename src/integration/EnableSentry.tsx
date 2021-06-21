6090d87bbaf8c2bb58acab0f75c14e59f5bed346
Nikita Livyk <livik.nick@gmail.com>
2021-06-21 18:14:49 +0300

    FENGH-76: Added release prop to "EnableSentry" to link sourcemap and errors with sentry release version

--- src/integration/EnableSentry.tsx
+++ src/integration/EnableSentry.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react'
 import * as Sentry from '@sentry/react'
+import React, { useEffect } from 'react'
 
 type EnableSentryProps = {
   sentryDSN: string
   user?: { email: string }
+  release?: string
 }
 
-const EnableSentry = ({ sentryDSN, user }: EnableSentryProps): JSX.Element => {
+const EnableSentry = ({ sentryDSN, user, release }: EnableSentryProps): JSX.Element => {
   useEffect(() => {
     if (sentryDSN) {
-      Sentry.init({ dsn: sentryDSN })
+      Sentry.init({ dsn: sentryDSN, release })
       Sentry.configureScope((scope: Sentry.Scope) => scope.setUser({ email: user && user.email }))
     }
   }, [user, sentryDSN])
