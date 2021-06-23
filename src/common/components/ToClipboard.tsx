5839bd86462ca7e5338cca19185d6a33f7842a33
Nikita Livyk <n.livyk@bestdoctor.ru>
2021-06-23 10:42:17 +0300

    SVCT-619: implement copy to clipboard in WidgetWrapper using ToClipboard

--- src/common/components/ToClipboard.tsx
+++ src/common/components/ToClipboard.tsx
@@ -1,35 +1,33 @@
-import { Box } from '@chakra-ui/react'
+import { Box, BoxProps } from '@chakra-ui/react'
 import type { BaseNotifier } from 'common/notifier'
 import React, { useCallback } from 'react'
 import { Copy } from 'react-feather'
-import styled from 'styled-components'
 
-type ButtonProps = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'value' | 'onClick'>
-
-interface ToClipboardProps extends ButtonProps {
-  value?: string
+interface ToClipboardProps extends BoxProps {
+  value?: string | Function
   notifier?: BaseNotifier
   onClick?: () => void
 }
 
-const Root = styled.button`
-  display: flex;
-  align-items: center;
-`
-
 export const ToClipboard = ({ value, onClick, notifier, children, ...rest }: ToClipboardProps): JSX.Element => {
   const handleClick = useCallback(() => {
-    if (value) {
-      navigator.clipboard.writeText(value)
+    let realValue: string | undefined
+    if (typeof value === 'function') {
+      realValue = value()
+    } else {
+      realValue = value
+    }
+    if (realValue) {
+      navigator.clipboard.writeText(realValue)
       if (notifier) notifier.notifySuccess('Скопировано в буфер обмена')
     }
     if (onClick) onClick()
   }, [onClick, notifier, value])
 
   return (
-    <Root onClick={handleClick} {...rest}>
-      <Box className="icon" as={Copy} size="1em" mr={3} display="inline" color="blue.500" />
-      {children}
-    </Root>
+    <Box display="flex" alignItems="center" onClick={handleClick} {...rest}>
+      <Box className="icon" as={Copy} size="1em" display="inline" color="blue.500" />
+      {!!children && <Box ml={3}>{children}</Box>}
+    </Box>
   )
 }
