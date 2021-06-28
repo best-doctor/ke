4f33d5e22171144d35dc0333bf8bb97cfb588886
Nikita Livyk <n.livyk@bestdoctor.ru>
2021-06-28 12:00:19 +0300

    feat: add the ability to pass to EnableSentry config

--- src/integration/EnableSentry.tsx
+++ src/integration/EnableSentry.tsx
@@ -4,15 +4,16 @@ import * as Sentry from '@sentry/react'
 type EnableSentryProps = {
   sentryDSN: string
   user?: { email: string }
+  config?: Omit<Sentry.BrowserOptions, 'dsn'>
 }
 
-const EnableSentry = ({ sentryDSN, user }: EnableSentryProps): JSX.Element => {
+const EnableSentry = ({ sentryDSN, user, config }: EnableSentryProps): JSX.Element => {
   useEffect(() => {
     if (sentryDSN) {
-      Sentry.init({ dsn: sentryDSN })
+      Sentry.init({ ...config, dsn: sentryDSN })
       Sentry.configureScope((scope: Sentry.Scope) => scope.setUser({ email: user && user.email }))
     }
-  }, [user, sentryDSN])
+  }, [user, sentryDSN, config])
 
   return <></>
 }
