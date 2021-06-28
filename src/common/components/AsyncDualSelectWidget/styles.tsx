3ec341541f6f004706c18cefe3fb37f3f31535d8
arhont.js <arhont.js@gmail.com>
2021-06-28 08:57:43 +0300

    FENG-44 'Multiselect' from two lists

--- src/common/components/AsyncDualSelectWidget/styles.tsx
+++ src/common/components/AsyncDualSelectWidget/styles.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { Box, Button, Text } from '@chakra-ui/react'
+import { CloseIcon, SearchIcon } from '@chakra-ui/icons'
 
 const defaultSideContainerWidth = '200px'
 const defaultContainerHeight = '250px'
@@ -7,12 +8,17 @@ const defaultButtonWidth = '140px'
 
 const SideContainer = styled(Box)`
   display: flex;
+  user-select: none;
   flex-direction: column;
   width: ${defaultSideContainerWidth};
   height: ${defaultContainerHeight};
 `
 
 const SideContainerTitle = styled(Text)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   padding: 10px;
   border-top: 1px solid #e0e0e0;
   border-left: 1px solid #e0e0e0;
@@ -32,4 +38,28 @@ const SelectButton = styled(Button)`
   width: ${defaultButtonWidth};
   margin-bottom: 5px;
 `
-export { CenterContainer, SideContainer, SelectButton, SideContainerTitle }
+
+const StyledSearchIcon = styled(SearchIcon)`
+  cursor: pointer;
+  transition: color 0.2s linear;
+  margin-left: 5px;
+
+  &:hover {
+    color: dodgerblue;
+  }
+`
+
+const StyledCloseIcon = styled(CloseIcon)`
+  cursor: pointer;
+  color: dodgerblue !important;
+  transition: color 0.2s linear;
+  margin-left: 5px;
+
+  &:hover {
+    &:hover {
+      color: crimson !important;
+    }
+  }
+`
+
+export { CenterContainer, SideContainer, SelectButton, SideContainerTitle, StyledSearchIcon, StyledCloseIcon }
