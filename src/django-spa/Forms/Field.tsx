42e2fcf2f3c4fbe5941197138f3771e4a00805eb
Nikita Livyk <n.livyk@bestdoctor.ru>
2021-06-30 15:39:16 +0300

    feat: created Full layout. Old Full layout moved to WithError layout
    feat: pass label to layout map of the Field

--- src/django-spa/Forms/Field.tsx
+++ src/django-spa/Forms/Field.tsx
@@ -1,30 +1,34 @@
-import React, { ElementType, RefObject, useCallback, useRef } from 'react'
+import React, { ElementType, ReactElement, RefObject, useCallback, useRef } from 'react'
 import { ControlRefProps, useField, useFieldValidation, ValidationResult, Validator } from '@cdk/Forms'
 import { makeWithLayout, PropsWithDefaultLayout } from '@cdk/Layouts'
 
 import type { NodeProps } from './types'
 import { Simple } from './Layouts'
 
-export const Field = makeWithLayout(({ name, as, validator, ...other }: FieldProps<unknown, ControlProps<unknown>>) => {
-  const controlRef = useRef<ControlRefProps>(null)
-  const { value, onChange } = useField(name, controlRef)
-  const { errors, validate } = useFieldValidation(name, value, validator || defaultValidator)
+export const Field = makeWithLayout(
+  ({ name, as, validator, label, ...other }: FieldProps<unknown, ControlProps<unknown>>) => {
+    const controlRef = useRef<ControlRefProps>(null)
+    const { value, onChange } = useField(name, controlRef)
+    const { errors, validate } = useFieldValidation(name, value, validator || defaultValidator)
 
-  const handleChange = useCallback(
-    async (v: unknown) => {
-      await validate(v)
-      onChange(v)
-    },
-    [onChange, validate]
-  )
+    const handleChange = useCallback(
+      async (v: unknown) => {
+        await validate(v)
+        onChange(v)
+      },
+      [onChange, validate]
+    )
 
-  // Don't found why, but type declaration necessary here https://github.com/microsoft/TypeScript/issues/28631#issuecomment-477240245
-  const Component: ElementType = as
-  return {
-    Control: <Component ref={controlRef} value={value} onChange={handleChange} {...other} />,
-    Errors: errors && errors.length ? errors[0].message : '',
-  }
-}, Simple) as <T, P extends ControlProps<T>>(
+    // Don't found why, but type declaration necessary here https://github.com/microsoft/TypeScript/issues/28631#issuecomment-477240245
+    const Component: ElementType = as
+    return {
+      Control: <Component ref={controlRef} value={value} onChange={handleChange} {...other} />,
+      Errors: errors && errors.length ? errors[0].message : '',
+      Label: label,
+    }
+  },
+  Simple
+) as <T, P extends ControlProps<T>>(
   props: PropsWithDefaultLayout<FieldProps<T, P>, { Control: JSX.Element }>
 ) => JSX.Element
 
@@ -43,4 +47,7 @@ interface BaseFieldProps<T, P> extends NodeProps {
   validator?: Validator
 }
 
-type FieldProps<T, P> = BaseFieldProps<T, P> & Omit<P, keyof BaseFieldProps<T, P> | keyof ControlProps<T>>
+type FieldProps<T, P> = BaseFieldProps<T, P> &
+  Omit<P, keyof BaseFieldProps<T, P> | keyof ControlProps<T>> & {
+    label?: ReactElement
+  }
