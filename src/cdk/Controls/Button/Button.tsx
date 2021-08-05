ab0c6e9d0c4d19bea394f6ada67d2a39db2f5ec3
d.nemkov <d.nemkov@MacBook-Pro-Admin.local>
2021-08-05 17:51:52 +0500

    fix: fix state update after Button unmount

--- src/cdk/Controls/Button/Button.tsx
+++ src/cdk/Controls/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Button as ChakraButton } from '@chakra-ui/react'
 import type { ButtonProps } from '@chakra-ui/button/dist/types/button'
 
@@ -8,15 +8,30 @@ import { ensurePromise } from '../../../DetailView/utils/dataAccess'
 export const Button = (props: ButtonProps): JSX.Element => {
   const { onClick, isDisabled: initialDisabled } = props
   const [isDisabled, setIsDisabled] = usePropState<boolean>(initialDisabled || false)
+  const isMounted = useRef(false)
 
   const handleClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<void> => {
     if (onClick) {
       setIsDisabled(true)
       return ensurePromise(onClick(e)).finally(() => {
-        setIsDisabled(false)
+        /**
+         * Promise can resolve after component is unmounted,
+         * so we ensure that we set state while component is mounted.
+         */
+        if (isMounted.current) {
+          setIsDisabled(false)
+        }
       })
     }
   }
+
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
 
   return <ChakraButton {...props} isDisabled={isDisabled} onClick={handleClick} />
 }
