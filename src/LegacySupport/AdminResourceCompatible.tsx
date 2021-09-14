34ccc283deee37ac173fc8c819f1a20bbefb3362
Nikita Livyk <n.livyk@bestdoctor.ru>
2021-09-14 16:36:31 +0300

    feat: add dynamic title for admin resource compatible

--- src/LegacySupport/AdminResourceCompatible.tsx
+++ src/LegacySupport/AdminResourceCompatible.tsx
@@ -1,11 +1,15 @@
 import React, { PropsWithChildren } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import { ErrorBoundary } from '../common/components/ErrorBoundary'
 
-export function AdminResourceCompatible({ path, children }: AdminResourceCompatibleProps): JSX.Element {
+export function AdminResourceCompatible({ navTitle, path, children }: AdminResourceCompatibleProps): JSX.Element {
   return (
     <Switch>
       <Route exact strict path={path}>
+        <Helmet>
+          <title>{navTitle}</title>
+        </Helmet>
         <ErrorBoundary>{children}</ErrorBoundary>
       </Route>
     </Switch>
