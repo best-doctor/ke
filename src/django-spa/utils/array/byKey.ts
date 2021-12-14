1837207022756c3666dd651fcf1a1b930d4701fc
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2021-12-15 02:57:47 +0300

    refactor: добавлена документация для утилит

--- src/django-spa/utils/array/byKey.ts
+++ src/django-spa/utils/array/byKey.ts
@@ -1,3 +1,22 @@
+/**
+ * Проходит по массиву словарей и возвращает массив значений по переданному ключу
+ *
+ * @example
+ * ```
+ * const result = byKey([{
+ *  a: 1,
+ *  b: false,
+ * }, {
+ *   a: 2,
+ *   b: true,
+ * }], 'a')
+ *
+ * // result === [1, 2]
+ * ```
+ *
+ * @param arr - исходный массив однотипных словарей
+ * @param key - ключ, по которому из словарей будут извлечены данные
+ */
 export function byKey<T, K extends keyof T>(arr: T[], key: K): T[K][] {
   return arr.map((v) => v[key])
 }
--- src/django-spa/utils/array/compact.ts
+++ src/django-spa/utils/array/compact.ts
@@ -1,3 +1,15 @@
+/**
+ * Создаёт новый массив, вычищая из исходного все falsy значения
+ *
+ * @example
+ * ```
+ * const result = compact([1, 0, '', 'a', false, null, undefined, true, []])
+ *
+ * // result === [1, 'a', true, []]
+ * ```
+ *
+ * @param arr - исходный массив
+ */
 export function compact<T>(arr: readonly T[]): Exclude<T, undefined | null | false>[] {
   return arr.filter((v) => !!v) as Exclude<T, undefined | null | false>[]
 }
