60eedbab958e46971cc984a3e2b118e9b5939d0c
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2021-12-20 01:49:14 +0300

    refactor: удаление неиспользуемых типов

--- src/django-spa/cdk/multiple-contexts/types.ts
+++ src/django-spa/cdk/multiple-contexts/types.ts
@@ -1,4 +1,4 @@
-import { Context, FC, PropsWithChildren, ReactElement } from 'react'
+import { Context, PropsWithChildren, ReactElement } from 'react'
 import { PolymorphProps } from '@cdk/types'
 
 export type ContextsRecord = Record<string, Context<any>>
@@ -8,19 +8,6 @@ export type ContextData<Ctx> = Ctx extends Context<infer T> ? T : never
 export type ContextsData<Contexts extends ContextsRecord> = {
   [K in keyof Contexts]: ContextData<Contexts[K]>
 }
-
-export type ContextsControl<Contexts extends ContextsRecord, RootProps = ContextsData<Contexts>> = [
-  root: FC<RootProps>,
-  makeConsumer: ConsumerMaker<Contexts>
-]
-
-export type ConsumerMaker<Contexts extends ContextsRecord> = <
-  K extends keyof Contexts,
-  ProxiedProps = Pick<ContextsData<Contexts>, K>
->(
-  contextKeys: K[],
-  proxy?: (ctx: Pick<ContextsData<Contexts>, K>) => ProxiedProps
-) => PolymorphComponent<ProxiedProps>
 
 export type PolymorphComponent<RequiredProps> = <TargetProps extends RequiredProps>(
   props: PropsWithChildren<PolymorphProps<RequiredProps, TargetProps>>
