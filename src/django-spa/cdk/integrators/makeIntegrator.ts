7ed24679e690481786b6c09bd3684d39245f64a8
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2021-12-20 03:20:10 +0300

    docs: добавлена документация для @cdk/integrators

--- src/django-spa/cdk/integrators/makeIntegrator.ts
+++ src/django-spa/cdk/integrators/makeIntegrator.ts
@@ -3,6 +3,28 @@ import { mapKey } from '@utils/dicts'
 
 import { Integrator } from './types'
 
+/**
+ * Создаёт новый объект, объединяя корневой компонент с вложенными. Прямой доступ
+ * к новому объекту использует переданный корневой компонент, вложенные же
+ * компоненты доступны в качестве полей объекта. Ключи предварительно прогоняются
+ * через capitalize. Какой-либо дополнительной магии не происходит и фактически
+ * так объединять можно произвольные объекты.
+ *
+ * @example
+ * ```
+ * const Integrator = makeIntegrator(Root, { filters: Filters, someData: Data })
+ *
+ * // <Integrator {...rootProps}>
+ * //   <div>
+ * //     <Integrator.SomeData {...dataProps} />
+ * //   </div>
+ * //   <Integrator.Filters {...filtersProps} />
+ * // </Integrator>
+ * ```
+ *
+ * @param root - корневой компонент
+ * @param inners - словарь вложенных компонентов
+ */
 export function makeIntegrator<Root extends ComponentType, Inners extends Record<string, ComponentType>>(
   root: Root,
   inners: Inners
