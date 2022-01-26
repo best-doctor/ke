6f705461187b03e112ba9497be8a42158f7f8a8e
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2022-01-27 01:44:01 +0300

    docs: tsdoc для makeFakeContent

--- src/django-spa/test-utils/fake-components/makeFakeContent.ts
+++ src/django-spa/test-utils/fake-components/makeFakeContent.ts
@@ -1,5 +1,11 @@
 import stringify from 'safe-stable-stringify'
 
+/**
+ * Генерит стабильную строку на основе входных параметров
+ *
+ * @param componentName - имя компонента
+ * @param props - параметры компонента
+ */
 export function makeFakeContent(componentName: string, props: unknown): string {
   return `Component "${componentName}", props: "${stringify(props)}"`
 }
