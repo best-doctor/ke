a47ce5d9a29430d39a3a3f1c15438d057025b9ac
Alexander Bezrukov <a.bezrukov@bestdoctor.ru>
2022-03-15 09:33:38 +0300

    Revert "fix: подписываем DateWidget на изменения в store для корректного обновления"
    This reverts commit b50a97963b9cd2c9501eef866ae477e4f894fa39.

--- src/DetailView/widgets/DateWidget.tsx
+++ src/DetailView/widgets/DateWidget.tsx
@@ -12,7 +12,6 @@ import { handleUserAction } from '../../common/utils/handleUserAction'
 import type { Accessor, OptionalDate, WidgetProps } from '../../typing'
 import { useCreateTestId } from '../../django-spa/aspects'
 import { getAccessor } from '../utils/dataAccess'
-import { useStore } from 'effector-react'
 
 const eventName = EventNameEnum.DATETIME_CHANGE
 const widgetType = WidgetTypeEnum.INPUT
@@ -54,7 +53,7 @@ const DateWidget = (props: DateWidgetProps): JSX.Element => {
     mainDetailObject,
   } = props
 
-  const context = useStore(containerStore)
+  const context = containerStore.getState()
   const { targetUrl, content, isRequired, widgetDescription } = useWidgetInitialization(
     { ...props, context },
     { allowAllDefinedValues: getAccessor(allowAllDefinedValues) }
@@ -67,8 +66,10 @@ const DateWidget = (props: DateWidgetProps): JSX.Element => {
 
   const handleChange = (value: OptionalDate): void => {
     const widgetValue = value ? format(value, 'yyyy-MM-dd') : ''
+
     handleUserAction({ ...props, widgetValue, targetUrl, eventName, widgetType })
   }
+
   const { getDataTestId } = useCreateTestId()
   return (
     <WidgetWrapper
