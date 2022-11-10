98148a5e2958f15b6e73e73777a29298751db684
LerikP <lerikpav@gmail.com>
2022-11-10 16:25:01 +0300

    feat: improve new wizard typing

--- src/Wizard/types.ts
+++ src/Wizard/types.ts
@@ -1,28 +1,32 @@
 import { ReactNode } from 'react'
 
-interface WizardProps<Key extends string, Action extends string> {
+interface WizardProps<Key extends string, Action extends string, StepData extends object = {}> {
   map: Record<Key, Partial<Record<Action, Key>>>
-  steps: Record<Key, (args: StepArgs<Action>) => ReactNode>
+  steps: Record<Key, (args: StepArgs<Action, StepData>) => ReactNode>
   start: Start<Key>
   name: string
-  onStepChange: (stepKey: Key, data: object) => void
-  onFinish: (data: object) => void
+  onStepChange: (stepKey: Key, data: StepData) => void
+  onFinish: (data: StepData) => void
   onRestart: () => void
 }
 
-interface Start<Key> {
+interface Start<Key, StepData extends object = {}> {
   step: Key
-  data: object
+  data: StepData
 }
 
-interface StepArgs<Action> {
-  data: object
-  next: NextStep<Action>
+interface StepArgs<Action, StepData extends object = {}> {
+  data: StepData
+  next: NextStep<Action, StepData>
   restart: () => void
   finish: () => void
-  onChange: (data: object) => void
+  onChange: (data: StepData) => void
 }
 
-type NextStep<Action> = (nextKey: Action, data: object, submit?: (data: unknown) => void) => void
+type NextStep<Action, StepData extends object = {}> = (
+  nextKey: Action,
+  data: StepData,
+  submit?: (data: unknown) => void
+) => void
 
 export { WizardProps, NextStep, StepArgs }
